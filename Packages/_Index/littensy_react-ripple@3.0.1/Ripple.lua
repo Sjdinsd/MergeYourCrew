c9dce9d19f5a30536a4acf12a739368cd32abb20
Sjdinsd <sjdinsd@gmail.com>
2026-01-25 17:21:51 -0800

    first commit

---
+++ Packages/_Index/littensy_react-ripple@3.0.1/Ripple.lua
@@ -0,0 +1,12 @@
+local REQUIRED_MODULE = require(script.Parent.Parent["littensy_ripple@0.10.1"]["ripple"])
+export type Animatable = REQUIRED_MODULE.Animatable 
+export type AnimatableType = REQUIRED_MODULE.AnimatableType 
+export type AnimatablePrimitive = REQUIRED_MODULE.AnimatablePrimitive 
+export type Easing = REQUIRED_MODULE.Easing 
+export type Motion<T > = REQUIRED_MODULE.Motion<T >
+export type MotionOptions<T > = REQUIRED_MODULE.MotionOptions<T >
+export type Spring<T > = REQUIRED_MODULE.Spring<T >
+export type SpringOptions<T > = REQUIRED_MODULE.SpringOptions<T >
+export type Tween<T > = REQUIRED_MODULE.Tween<T >
+export type TweenOptions<T > = REQUIRED_MODULE.TweenOptions<T >
+return REQUIRED_MODULE
---
+++ Packages/react-ripple.lua
@@ -0,0 +1,10 @@
+local REQUIRED_MODULE = require(script.Parent._Index["littensy_react-ripple@3.0.1"]["react-ripple"])
+export type Animatable = REQUIRED_MODULE.Animatable 
+export type Easing = REQUIRED_MODULE.Easing 
+export type Motion<T > = REQUIRED_MODULE.Motion<T >
+export type MotionOptions<T > = REQUIRED_MODULE.MotionOptions<T >
+export type Spring<T > = REQUIRED_MODULE.Spring<T >
+export type SpringOptions<T > = REQUIRED_MODULE.SpringOptions<T >
+export type Tween<T > = REQUIRED_MODULE.Tween<T >
+export type TweenOptions<T > = REQUIRED_MODULE.TweenOptions<T >
+return REQUIRED_MODULE
