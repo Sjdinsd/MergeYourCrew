11ebf777dab09d45bbcf1e40b763d9ad8936ab14
Sjdinsd <sjdinsd@gmail.com>
2026-01-31 21:36:55 -0800

    Package Updates

--- Packages/dataservice.lua
+++
@@ -1,4 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["leifstout_dataservice@0.4.0"]["dataservice"])
-export type Server = REQUIRED_MODULE.Server 
-export type Client = REQUIRED_MODULE.Client 
-return REQUIRED_MODULE
